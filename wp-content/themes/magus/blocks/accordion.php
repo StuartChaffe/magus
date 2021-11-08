1ef617436fe166f1bbdef87b9e848cea459b4168
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-11-08 15:56:40 +0000

    Add all blocks

---
+++ wp-content/themes/magus/blocks/accordion.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* The template used for displaying an accordion.
+*/
+
+?>
+
+<section class="accordion">
+	Accordion
+</section>
---
+++ wp-content/themes/magus/blocks/banner.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* The template used for displaying a banner.
+*/
+
+?>
+
+<section class="banner">
+	Banner
+</section>
