9c7cc206570c6966b925f21a14496f1d8bfc172b
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-11-29 17:36:49 +0000

    Add small to title

--- wp-content/themes/magus/blocks/accordion.php
+++ wp-content/themes/magus/blocks/accordion.php
@@ -10,11 +10,12 @@
 			<ul class="accordion">
 			<?php while( have_rows('accordion') ): the_row();
 				$title = get_sub_field('accordion_title', false, false);
+				$subtitle = get_sub_field('accordion_title_small', false, false);
 				$content = get_sub_field('accordion_content');
 				$image = get_sub_field('accordion_logo');
 			?>
 				<li>
-					<button class="accordion--title" aria-expanded="false"><?php echo $title ?> <svg class="icon icon--open"><use xlink:href="#open"></use></svg><svg class="icon icon--close"><use xlink:href="#close"></use></svg></button>
+					<button class="accordion--title" aria-expanded="false"><?php echo $title ?><?php if ( $subtitle ) { ?><span><?php echo $subtitle ?></span><?php } ?> <svg class="icon icon--open"><use xlink:href="#open"></use></svg><svg class="icon icon--close"><use xlink:href="#close"></use></svg></button>
 					<div class="accordion--content">
 					<?php if ( $image ) { ?>
 						<div class="accordion--content-image">
