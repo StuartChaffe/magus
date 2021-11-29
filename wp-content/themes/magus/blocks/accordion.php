0cedbbc8b53b9112b90d8887b15b91ec9c1a96fb
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-11-29 14:35:33 +0000

    Add accordion

--- wp-content/themes/magus/blocks/accordion.php
+++ wp-content/themes/magus/blocks/accordion.php
@@ -2,9 +2,32 @@
 /**
 * The template used for displaying an accordion.
 */
-
+// TITLE? $content = get_field( 'accordion_content');
 ?>
-
-<section class="accordion">
-	Accordion
-</section>
+<div class="container">
+	<section class="accordion">
+		<?php if( have_rows('accordion') ): ?>
+			<ul class="accordion">
+			<?php while( have_rows('accordion') ): the_row();
+				$title = get_sub_field('accordion_title', false, false);
+				$content = get_sub_field('accordion_content');
+				$image = get_sub_field('accordion_logo');
+			?>
+				<li>
+					<button class="accordion--title" aria-expanded="false"><?php echo $title ?> <svg class="icon icon--open"><use xlink:href="#open"></use></svg><svg class="icon icon--close"><use xlink:href="#close"></use></svg></button>
+					<div class="accordion--content">
+					<?php if ( $image ) { ?>
+						<div class="accordion--content-image">
+							<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+						</div>
+					<?php } ?>
+						<div class="accordion--content-text">
+							<?php echo $content ?>
+						</div>
+					</div>
+				</li>
+			<?php endwhile; ?>
+			</ul>
+		<?php endif; ?>
+	</section>
+</div>
