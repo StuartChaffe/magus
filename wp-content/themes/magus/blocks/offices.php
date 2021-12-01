2df34a2534e22e5082d3a45a57d79ffec0dc1542
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-12-01 11:41:44 +0000

    Add office list

--- wp-content/themes/magus/blocks/offices.php
+++ wp-content/themes/magus/blocks/offices.php
@@ -4,7 +4,22 @@
 */
 
 ?>
-
-<section class="office-list">
-	Office list
-</section>
+<div class="container">
+	<section class="office-list">
+		<div class="link-list">
+		<?php while( have_rows('offices') ): the_row();
+			$image = get_sub_field('office_image');
+			$content = get_sub_field('office_content');
+		?>
+			<div class="link-list-item">
+				<div class="link-list-item--image">
+					<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+				</div>
+				<div class="link-list-item--content">
+					<?php echo $content; ?>
+				</div>
+			</div>
+		<?php endwhile; ?>
+		</div>
+	</section>
+</div>
