aae0886f918a0bca7c113a2c30265252b65c04aa
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-12-02 08:29:06 +0000

    Fix article banner image

--- wp-content/themes/magus/single.php
+++ wp-content/themes/magus/single.php
@@ -3,16 +3,16 @@
 <?php while ( have_posts() ) : the_post(); ?>
 <?php
 	$image = get_field('banner_image', get_the_ID());
-	$imagesquare = get_field('square_image', get_the_ID());
+	$imagemobile = get_field('square_image', get_the_ID());
 	$author = get_field('author', get_the_ID());
 ?>
 <?php if ($image) { ?>
 
 <section class="banner">
 	<!-- <div class="banner--image" style="background-image: url('<?php echo $image['url']; ?>')"> -->
-	<div class="banner--image banner--image__single">
+	<div class="banner--image banner--image__single" style="background-image: url('<?php echo $image['url']; ?>')">
 		<img loading="lazy" class="hidemobile" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-		<img loading="lazy" class="hidedesktop" src="<?php echo $imagesquare['url']; ?>" alt="<?php echo $imagesquare['alt']; ?>" />
+		<?php if ( $imagemobile ) { ?><img loading="lazy" class="hidedesktop" src="<?php echo $imagemobile['url']; ?>" alt="<?php echo $imagemobile['alt']; ?>" /><?php } ?>
 	</div>
 	<div class="container">
 		<div class="banner--title">
