000108818751973f0d06841d2a63e3dd8e7fd235
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-12-02 14:31:14 +0000

    Add career post type

--- wp-content/themes/magus/blocks/careers.php
+++ wp-content/themes/magus/blocks/careers.php
@@ -1,10 +1,34 @@
 <?php
 /**
-* The template used for displaying careers.
+* The template used for displaying all team members.
 */
-
 ?>
+<?php
+	$career = new WP_Query( array(
+		'post_type' => 'careers',
+		'posts_per_page' => -1,
+		'orderby' => 'date',
+		'order' => 'DESC'
+	));
+?>
+<div class="container">
+	<section class="careers">
+			<?php while($career->have_posts()) : $career->the_post(); ?>
 
-<section class="careers">
-	Careers
-</section>
+			<?php
+				$position = get_field('job_position', get_the_ID());
+				$location = get_field('job_location', get_the_ID());
+				$description = get_field('job_description', get_the_ID());
+			?>
+			<div class="careers-item">
+				<h2><?php the_title(); ?></h2>
+				<p><span>Position:</span><?php echo $position; ?></p>
+				<p><span>Location:</span><?php echo $location; ?></p>
+				<p><span>Description:</span><?php echo $description; ?></p>
+
+				<a class="btn" href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>">View Job Specifications</a>
+			</div>
+
+			<?php endwhile; wp_reset_query(); ?>
+	</section>
+</div>
