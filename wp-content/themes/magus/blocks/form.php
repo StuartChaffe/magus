a95dce811ade80b3e22fd80c62398dfde61ce8c6
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-12-09 21:26:57 +0000

    Style form

--- wp-content/themes/magus/blocks/form.php
+++ wp-content/themes/magus/blocks/form.php
@@ -2,67 +2,11 @@
 /**
 * The template used for displaying a form block.
 */
-
+$content = get_field('form_content');
 ?>
 
 <section class="form">
-<div class="container">
-	<!-- Begin Mailchimp Signup Form -->
-	<div id="mc_embed_signup">
-	<form action="https://maguswealth.us20.list-manage.com/subscribe/post?u=1a8514951538f51b496cb0c30&amp;id=2bdee22ad7" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-		<div id="mc_embed_signup_scroll">
-	<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-	<div class="mc-field-group">
-		<label for="mce-FNAME">First Name  <span class="asterisk">*</span>
-	</label>
-		<input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
+	<div class="container">
+		<?php echo $content ?>
 	</div>
-	<div class="mc-field-group">
-		<label for="mce-LNAME">Last Name  <span class="asterisk">*</span>
-	</label>
-		<input type="text" value="" name="LNAME" class="required" id="mce-LNAME">
-	</div>
-	<div class="mc-field-group">
-		<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
-	</label>
-		<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-	</div>
-	<div class="mc-field-group">
-		<label for="mce-TITLE">Title  <span class="asterisk">*</span>
-	</label>
-		<input type="text" value="" name="TITLE" class="required" id="mce-TITLE">
-	</div>
-	<div class="mc-field-group">
-		<label for="mce-HELP">How can we help you?  <span class="asterisk">*</span>
-	</label>
-		<select name="HELP" class="required" id="mce-HELP">
-		<option value=""></option>
-		<option value="Creating your financial plan">Creating your financial plan</option>
-	<option value="Investment and wealth management">Investment and wealth management</option>
-	<option value="Cash flow planning">Cash flow planning</option>
-	<option value="Retirement planning">Retirement planning</option>
-	<option value="Tax planning">Tax planning</option>
-	<option value="Protection">Protection</option>
-	<option value="Business owners">Business owners</option>
-	<option value="Family consulting">Family consulting</option>
-
-		</select>
-	</div>
-	<div class="mc-field-group">
-		<label for="mce-ENQUIRY">Please provide a brief description of your enquiry  <span class="asterisk">*</span>
-	</label>
-		<input type="text" value="" name="ENQUIRY" class="required" id="mce-ENQUIRY">
-	</div>
-		<div id="mce-responses" class="clear">
-			<div class="response" id="mce-error-response" style="display:none"></div>
-			<div class="response" id="mce-success-response" style="display:none"></div>
-		</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-		<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_1a8514951538f51b496cb0c30_2bdee22ad7" tabindex="-1" value=""></div>
-		<div class="clear"><input type="submit" value="Submit" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-		</div>
-	</form>
-	</div>
-<!--End mc_embed_signup-->
-</div>
-
 </section>
